978c850bea27298b93c5be7bd91d1d8eb8626293
moon <mato7313@gmail.com>
2019-11-01 16:08:58 +0900

    No.13 Roman to Integer first

---
+++ Easy/13.RomanToInteger.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let result = 0;
+
+    let characterList = s.split("");
+
+    
+    for(let i=0; i < s.length; i++) {
+      // if(s[i]
+    }
+
+
+
+    result = characterList;
+
+
+
+    return result;
+};
+
+
+console.log(romanToInt("III"));
---
+++ README.md
@@ -0,0 +1,5 @@
+# LeetCode Problems Solving
+
+## Problems List
+#### Easy
+- 13. Roman to Integer
