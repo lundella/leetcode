806a64bd3011e6b6137cae52038999df07b7b2c0
moon <mato7313@gmail.com>
2019-11-04 14:05:26 +0900

    compare first and next roman strings

--- Easy/13.RomanToInteger.js
+++ Easy/13.RomanToInteger.js
@@ -3,18 +3,42 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    let result = 0;
+    const romanSet = {
+      "I": 1,
+      "V": 5,
+      "X": 10,
+      "L": 50,
+      "C": 100,
+      "D": 500,
+      "M": 1000
+    }
+    let result = [];
+    let ii = 0;
 
     let characterList = s.split("");
 
+    let translateInt = characterList.map((char, index)=>{
+      return romanSet[char];
+    })
+
+    console.log(translateInt);
     
-    for(let i=0; i < s.length; i++) {
-      // if(s[i]
+    for(let i=0; i < characterList.length-1; i++) {
+      if(i === 0) { result += translateInt[i]; }
+
+      if(translateInt[i] === translateInt[i+1]) {
+        result[ii] += translateInt[i+1];
+      } else if(translateInt[i] < translateInt[i+1]){
+        result[ii] = translateInt[i+1] - result[ii];
+        // ii++;
+      } else if(translateInt[i] > translateInt[i+1]){
+        result[ii] += translateInt[i+1];
+      }
     }
 
 
 
-    result = characterList;
+    // result = characterList;
 
 
 
@@ -22,4 +46,4 @@ var romanToInt = function(s) {
 };
 
 
-console.log(romanToInt("III"));
+console.log(romanToInt("MCMXCIV"));
