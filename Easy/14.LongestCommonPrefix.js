72956afabe33066d53028f9b28f07eec491ebf4f
Juyoung Moon <>
2019-11-06 22:43:25 +0900

    add some part

--- Easy/14.LongestCommonPrefix.js
+++ Easy/14.LongestCommonPrefix.js
@@ -3,5 +3,26 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
+  let minStringLength = Number.MAX_VALUE;
+  let shortestWord = "";
+  
+  strs.forEach((word, index)=>{
+    if(word.length < minStringLength) {
+      minStringLength = word.length;
+      shortestWord = word;
+      removePoint = index;
+    }
+  })
+  strs.splice(removePoint, 1);
+
+  
+
+  
+  // Array.prototype.forEach.call(shortestWord, (char)=>{
     
+  // })
+
+  return strs;
 };
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]))
