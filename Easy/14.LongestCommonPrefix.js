1b33507b180a3e924dfc3820de5a1069ba6327b9
Juyoung Moon <>
2019-11-07 23:59:08 +0900

    add solution not complete

--- Easy/14.LongestCommonPrefix.js
+++ Easy/14.LongestCommonPrefix.js
@@ -5,6 +5,11 @@
 var longestCommonPrefix = function(strs) {
   let minStringLength = Number.MAX_VALUE;
   let shortestWord = "";
+  let sameCharCount = 0;
+
+  if(!strs.length) {
+    return "";
+  }
   
   strs.forEach((word, index)=>{
     if(word.length < minStringLength) {
@@ -15,14 +20,22 @@ var longestCommonPrefix = function(strs) {
   })
   strs.splice(removePoint, 1);
 
-  
+  for(let index=0; index < shortestWord.length; index++) {
+    let count = 0;
+    strs.forEach((word) => {
+      if (word[index] === shortestWord[index]) { 
+        count++; 
+      }
+    })
+    if (count == strs.length) {
+      sameCharCount++;
+    } else if(count < strs.length) {
+      return shortestWord.substring(0, sameCharCount);
+    } else {
+      return "";
+    };
+  };
 
-  
-  // Array.prototype.forEach.call(shortestWord, (char)=>{
-    
-  // })
-
-  return strs;
 };
 
-console.log(longestCommonPrefix(["flower", "flow", "flight"]))
+console.log(longestCommonPrefix(["aba", "cba"]))
