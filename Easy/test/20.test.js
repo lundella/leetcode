806c1622b70a615c7da7908584d5ede0edb3aac6
moon <mato7313@gmail.com>
2019-11-11 10:17:54 +0900

    add test code of valide parentheses

---
+++ Easy/test/20.test.js
@@ -0,0 +1,20 @@
+const solution = require('../20.ValidParentheses');
+const testExample = [
+  {
+    input: [],
+    output: ""
+  }
+];
+
+
+describe.each(testExample)(
+  'ValidParentheses test',
+  (testCase) => {
+    let input = testCase.input;
+    let expected = testCase.output;
+
+    test(`Test [${input}] to be ${expected}`, () => {
+      expect(solution(input)).toBe(expected)
+    })
+  }
+)
