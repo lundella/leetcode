4159942031bdd01a04f81bde07a49fbf23413654
Juyoung Moon <mato7313@gmail.com>
2019-11-14 03:01:29 +0900

    add No.20 solution not complete

--- Easy/20.ValidParentheses.js
+++ Easy/20.ValidParentheses.js
@@ -3,7 +3,44 @@
  * @return {boolean}
  */
 var isValid = function (s) {
+  const Brackets = {
+    "{": "}",
+    "[": "]",
+    "(": ")"
+  }
+  let inputArray = s.split("");
+  let stackBrackets = [];
+  let result = false;
 
+  if(!s) {
+    return true;
+  }
+
+  // for(let i = 0; i < inputArray.length; i++) {
+  //   let bracket = inputArray[i];
+
+  //   if (stackBrackets.length && (bracket === Brackets[stackBrackets[stackBrackets.length - 1]])) {
+  //     stackBrackets.pop();
+  //   }
+
+  //   console.log(i);
+  //   if (Brackets[bracket]) {
+  //     stackBrackets.push(bracket);
+  //   }
+  // }
+  inputArray.forEach((bracket, index)=>{
+    if (stackBrackets.length && (bracket === Brackets[stackBrackets[stackBrackets.length - 1]])) { 
+      stackBrackets.pop();
+    }
+
+    if(Brackets[bracket]) {
+      stackBrackets.push(bracket);
+    }
+  });
+  result = stackBrackets.length? false: true;
+
+  return result;
 };
 
+console.log(isValid("]()"));
 module.exports = isValid;
