a89841433c90843ba4ccfc0d2649d16478d7cecb
Juyoung Moon <mato7313@gmail.com>
2019-11-25 00:24:53 +0900

    [solved] add solution & test case

--- Easy/21.MergeTwoSortedLists.js
+++ Easy/21.MergeTwoSortedLists.js
@@ -11,44 +11,45 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    let value1 = l1;
-    let value2 = l2;
-    let iterator = true;
-    let iterated = 1;
-    let result = [];
-    let returneformat = {}
+  let value1 = l1;
+  let value2 = l2;
+  let iterator = true;
+  let iterated = 0;
+  let result = [];
+  let returnformat = {};
 
-    while (iterator) {
-      if (value2 && (value1 === null || value2.val <= value1.val)) {
-        result.push(value2.val);
+  if(!value1 && !value2) {
+    return returnformat;
+  }
 
-        value2 = value2.next;
-      } else {
-        result.push(value1.val);
-        value1 = value1.next;
-      }
-      iterator = ((value1 === null) && (value2 === null)) ? false : true;
-      iterated += 1;
+  while (iterator) {
+    if (value2 && (value1 === null || value2.val <= value1.val)) {
+      result.push(value2.val);
+
+      value2 = value2.next;
+    } else {
+      result.push(value1.val);
+      value1 = value1.next;
+    }
+    iterator = ((value1 === null) && (value2 === null)) ? false : true;
+    iterated += 1;
+  }
+
+  for (let index = iterated-1; 0 <= index ; index--) {
+    let tempObjectFormat = {
+      val: result[index],
+      next: null
     }
 
-    for(let index = 0; index < iterated; index++) {
-      let tempObjectFormat = {
-        val: result[index],
-        next: null
-      }
-      if(!returneformat.val) {
-        returneformat = {
-          val: result[index],
-          next: null
-        }
-      } else if(returneformat.next === null){
-        returneformat.next = {
-          val: result[index],
-          next: null
-        }
-      }
+    if (!returnformat) {
+      returnformat = tempObjectFormat;
+    } else {
+      tempObjectFormat.next = returnformat;
+      returnformat = tempObjectFormat;
     }
-    return result;
+  }
+  return returnformat;
 };
 
+// console.log(mergeTwoLists({ val: 1, next: { val: 2, next: { val: 4, next: null } } },  { val: 1, next: { val: 3, next: { val: 4, next: null } } }))
 module.exports = mergeTwoLists
--- Easy/test/21.test.js
+++ Easy/test/21.test.js
@@ -2,25 +2,31 @@ const solution = require('../21.MergeTwoSortedLists');
 const testExample = [
   {
     input: [
-        {val: 1, next:{ val:2, next:{val:4, next: null}}}, 
-        {val: 1, next:{ val:3, next:{val:4, next: null}}}
+        {val:1, next:{ val:2, next:{val:4, next: null}}}, 
+        {val:1, next:{ val:3, next:{val:4, next: null}}}
     ],
-    output: {val: 1, next:{ val:1, next:{val:2, next: {val:3, next: {val:4, next: {val:4, next: null}}}}}}
-
+    output: {val:1, next:{ val:1, next:{val:2, next: {val:3, next: {val:4, next: {val:4, next: {}}}}}}}
   },
   {
     input: [
-      {val: 1, next:{ val:1, next:{val:1, next: null}}}, 
-      {val: 2, next:{ val:2, next:{val:3, next: {val:4, next: null}}}}
+      {val:1, next:{ val:1, next:{ val:1, next: null}}}, 
+      {val:2, next:{ val:2, next:{ val:3, next: { val:4, next: null}}}}
     ],
-    output: {val: 1, next:{ val:1, next:{val:1, next: {val:2, next: {val:2, next: {val:3, next: {val:4, next: null}}}}}}}
+    output: { val:1, next:{ val:1, next:{ val:1, next: { val:2, next: { val:2, next: { val:3, next: { val:4, next: {}}}}}}}}
   },
   {
     input: [
-      {val: 3, next:{ val:7, next:{val:8, next: null}}}, 
-      {val: 1, next:{ val:2, next:{val:3, next: {val:4, next: null}}}}
+      {val:3, next: { val:7, next:{ val:8, next: null}}}, 
+      {val:1, next: { val:2, next:{ val:3, next: { val:4, next: null}}}}
     ],
-    output: {val: 1, next:{ val:2, next:{val:3, next: {val:3, next: {val:4, next: {val:7, next: {val:8, next: null}}}}}}}
+    output: { val:1, next:{ val:2, next:{ val:3, next: { val:3, next: { val:4, next: { val:7, next: { val:8, next: {}}}}}}}}
+  },
+  {
+    input: [
+      {val:3, next: null}, 
+      {val:1, next: { val:2, next:{val:3, next: {val:4, next: null}}}}
+    ],
+    output: { val:1, next:{ val:2, next:{ val:3, next: { val:3, next: { val:4, next: {}}}}}}
   }
 ];
 
@@ -32,7 +38,7 @@ describe.each(testExample)(
     let expected = testCase.output;
 
     test(`Test [${input1, input2}] to be ${expected}`, () => {
-      expect(solution(input1, input2)).toMatchObject(expected);
+      expect(solution(input1, input2 )).toMatchObject(expected);
     })
   }
 )
