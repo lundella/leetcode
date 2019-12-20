dd59774202c062f45420a502014ec173ed8e142d
Juyoung Moon <mato7313@gmail.com>
2019-12-21 07:47:08 +0900

    [solved] add No.26 solution & test case

--- Easy/test/26.test.js
+++ Easy/test/26.test.js
@@ -1,20 +1,34 @@
 const solution = require('../26.RemoveDuplicatesFromSortedArray');
 const testExample = [
+  // Individual Test Case
   {
     input: [0, 0, 0, 1, 1, 2, 3],
-    output: 4
+    output: [0, 1, 2, 3]
+  },
+  {
+    input: [],
+    output: []
+  },
+  {
+    input: [0, 4, 4, 7, 7, 9],
+    output: [0, 4, 7, 9]
+  },
+  // Leetcode Test Case
+  {
+    input: [1, 1, 2],
+    output: [1, 2]
   }
 ];
 
 
 describe.each(testExample)(
-  'ValidParentheses test',
+  'RemoveDuplicatesFromSortedArray test',
   (testCase) => {
     let input = testCase.input;
     let expected = testCase.output;
 
     test(`Test [${input}] to be ${expected}`, () => {
-      expect(solution(input)).toBe(expected)
+      expect(solution(input)).toEqual(expected)
     })
   }
 )
