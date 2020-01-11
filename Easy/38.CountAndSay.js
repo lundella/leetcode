21f8833734070522a19976294163d65daf3eb309
Juyoung Moon <mato7313@gmail.com>
2020-01-12 08:03:53 +0900

    add solution not complete

--- Easy/38.CountAndSay.js
+++ Easy/38.CountAndSay.js
@@ -3,5 +3,35 @@
  * @return {string}
  */
 var countAndSay = function(n) {
-    
+  let previousResult = "11";
+  let sayResult = "";
+
+  if(n == 1) {
+    return "1";
+  } else if(n == 2) {
+    return "11";
+  }
+
+  for(var index = 2; index < n; index++) {
+    let temp = previousResult[0];
+    let count = 1;
+
+
+    for (var order = 1; order < (previousResult.length + 1); order++) {
+      if (previousResult[order] == temp) {
+        count++;
+      } else {
+        sayResult += count;
+        sayResult += temp;
+        temp = previousResult[order];
+        cout = 1;
+      }
+    }
+    previousResult = sayResult;
+    sayResult = "";
+    console.log(previousResult);
+  }
+  return previousResult;
 };
+
+countAndSay(6);
