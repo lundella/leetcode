d9514779efd54f05e96e5e22b837ac09f4413727
Juyoung Moon <mato7313@gmail.com>
2020-01-26 01:12:05 +0900

    [solved] add No.66 solution

--- Easy/66.PlusOne.js
+++ Easy/66.PlusOne.js
@@ -3,20 +3,20 @@
  * @return {number[]}
  */
 var plusOne = function (digits) {
+  function findIncreasePosition(digits, position) {
+    if ((digits[position] === 9) && (position)) {
+      digits[position] = 0;
+      findIncreasePosition(digits, position - 1);
+    } else {
+      digits[position] += 1;
+    }
+  }
+
+  findIncreasePosition(digits, (digits.length-1));
   let figure = digits.join('');
-  console.log(figure);
-
-  // figure = figure + 1;
-
   let result = figure.toString().split('');
-  console.log(figure);
 
   return result;
 };
 
-plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]);
-
 module.exports = plusOne;
-// 6145390195186705543
-// 6145390195186705000
-// 9007199254740991
--- Easy/test/66.test.js
+++ Easy/test/66.test.js
@@ -2,19 +2,23 @@ const solution = require('../66.PlusOne');
 const testExample = [
   {
     input: [4, 3, 2, 1],
-    output: [4, 3, 2, 2]
+    output: ['4', '3', '2', '2']
   },
   {
     input: [1, 2, 3],
-    output: [1, 2, 4]
+    output: ['1', '2', '4']
   },
   {
     input: [9],
-    output: [1, 0]
+    output: ['1', '0']
+  },
+  {
+    input: [9, 9],
+    output: ['1', '0', '0']
   },
   {
     input: [1, 2, 9],
-    output: [1, 2, 1, 0]
+    output: ['1', '3', '0']
   }
 ];
 
