c042a9f4e8d251c2096ffd9c7a5de2986a5aec92
Juyoung Moon <mato7313@gmail.com>
2020-02-23 15:29:02 +0900

    add Problems No.1

---
+++ Easy/1.TwoSum.js
@@ -0,0 +1,12 @@
+const twoSum = function (nums, target) {
+  const comp = {};
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (comp[nums[i]] >= 0) {
+      return [comp[nums[i]], i]
+    }
+    comp[target - nums[i]] = i;
+  }
+};
+
+module.exports = twoSum;
